c5a7fed589339e95f11479ebe4cdec8082d5a27a
Shubham Rai <shubham.rai@w3villa.com>
2025-06-12 15:20:51 +0530

    api responsce change

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,28 +3,36 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 // Register
+// Register
 exports.register = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
-    
+
     const existing = await User.findOne({ email });
     if (existing) return res.status(400).json({ message: 'User already exists' });
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = new User({
-      name,
-      email,
-      password: hashedPassword,
-      role
-    });
-
+    const newUser = new User({ name, email, password: hashedPassword, role });
     await newUser.save();
-    res.status(201).json({ message: 'User registered successfully ✅' });
+
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+    res.status(201).json({
+      message: 'User registered successfully ✅',
+      token,
+      user: {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        role: newUser.role
+      }
+    });
   } catch (error) {
     res.status(500).json({ message: 'Server error', error: error.message });
   }
 };
+
 
 // Login
 exports.login = async (req, res) => {
